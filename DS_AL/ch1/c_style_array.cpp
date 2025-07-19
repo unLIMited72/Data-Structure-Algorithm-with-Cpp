46e4493620f1d143acc41c8d8b0d2314db3a9547
lim_wsl <limcraft7260@gmail.com>
2025-07-19 13:39:43 -0700

    make some c style array example and .exe

---
+++ DS_AL/ch1/c_style_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int scores[5] = {50, 60, 70, 80, 90};
+
+    // int size_array = sizeof(scores) / sizeof(scores[0]);
+    int size_array = std::size(scores);
+    int sum = 0;
+    for (int i = 0; i < size_array; i++) {
+        sum += scores[i];
+    }
+
+    float mean = (float)sum / size_array;
+
+    std::cout << "Mean score : " << mean << std::endl;
+}
